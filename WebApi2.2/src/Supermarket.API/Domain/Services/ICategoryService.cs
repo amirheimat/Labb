29b98b1bb6f6b579e886e794556c614f0bd525c4
Amir Moemen <amir@heimat.se>
2019-08-21 16:29:34 +0200

    work

---
+++ WebApi2.2/src/Supermarket.API/Domain/Services/ICategoryService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Supermarket.API.Domain.Models;
+
+
+namespace Supermarket.API.Domain.Services
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<Category>> ListAsync();
+    }
+}
